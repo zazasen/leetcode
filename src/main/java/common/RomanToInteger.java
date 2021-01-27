cad88de82b90a1b9ea9f51cc46e8d976ec16a02f
chenzengsen09222 <chenzengsen09222@hellobike.com>
2021-01-27 16:52:59 +0800

    罗马数字转整数

---
+++ src/main/java/common/RomanToInteger.java
@@ -0,0 +1,60 @@
+package common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/roman-to-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        char[] chars = s.toCharArray();
+        int sum = 0;
+        int pre = getIValue(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            int temp = getIValue(chars[i]);
+            if(temp > pre){
+                sum -= pre;
+            }
+            if(temp <= pre){
+                sum += pre;
+            }
+            pre = temp;
+        }
+        sum += pre;
+        return sum;
+    }
+
+    public int getIValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+
+}
